63c852a7a223aad32041b771923c8f0b1387c9be
yewubin001 <931815516@qq.com>
2019-07-01 22:05:25 +0800

    vue demo commit

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Info from './views/Info.vue';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/info',
+      name: 'info',
+      component: Info,
     },
     {
       path: '/about',
---
+++ src/views/Info.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    hello info component
+    <button type="button" @click="add()">添加</button>
+  </div>
+</template>
+
+<script>
+  import store from '@/store';
+  export default {
+    name: 'Info',
+    store,
+    data() {
+      return {
+        msg: 'hello vue'
+      };
+    },
+    mounted() {
+      window.vue = this;
+    },
+    methods: {
+      add() {
+        console.log('add Event from info!');
+        // alert('alert from info')
+        debugger;
+        store.commit('increase');
+      },
+      output() {
+        console.log('in output');
+      }
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
